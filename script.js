3a681ab8bc6335cfe41b1de7c5f80e058bd1c2f8
SamuelDAlencar <sarl.2001@hotmail.com>
2021-12-07 19:17:44 -0300

    fifth requisition conclusion

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-// Constante 'cart' para uso geral no escopo global
+// Global scope variables
+const cartSec = document.querySelector('.cart');
 const cart = document.querySelector('.cart__items');
+const coinFormat = { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' };
+const totalPriceClass = '.total-price';
+let currentPrice = 0;
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -31,9 +35,23 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
+const priceSaver = (currentPriceInnerHtml, currentPriceNum) => {
+  localStorage.setItem('price', JSON.stringify(currentPriceInnerHtml));
+  localStorage.setItem('priceNum', JSON.stringify(currentPriceNum));
+};
+
 function cartItemClickListener({ target }) {
   target.remove();
   saveCartItems(cart.innerHTML);
+  const priceAfterClick = target.innerText.split('$')[1];
+  const totalPrice = document.querySelector(totalPriceClass);
+  totalPrice.innerText = `${(currentPrice - priceAfterClick)
+    .toLocaleString('pt-BR')
+    .replace('.', '')
+    .replace(',', '.')}`;
+  currentPrice -= priceAfterClick;
+  priceSaver(totalPrice.innerHTML, priceAfterClick);
+  console.log(priceAfterClick);
 }
 
 function createCartItemElement({ id: sku, title: name, price: salePrice }) {
@@ -57,25 +75,67 @@ const listCreator = async () => {
   }
 };
 
-const cartAdder = () => {
+const totalPriceCreator = () => {
+  const span = document.createElement('span');
+  cartSec.appendChild(span);
+  span.className = 'total-price';
+  const basePrice = 0;
+  span.innerText = `${basePrice
+    .toLocaleString('pt-BR')
+    .replace('.', '')
+    .replace(',', '.')}}`;
+};
+
+const priceCatcher = () => {
+  const totalPrice = document.querySelector(totalPriceClass);
+  const price = localStorage.getItem('price');
+  const priceNum = localStorage.getItem('priceNum');
+  if (!price) {
+    totalPrice.innerHTML = '0';
+  } else {
+    totalPrice.innerHTML = JSON.parse(price);
+  }
+  currentPrice = JSON.parse(priceNum);
+};
+
+const priceSum = (price) => {
+  const totalPrice = document.querySelector(totalPriceClass);
+  totalPrice.innerText = `${(currentPrice + price)
+    .toLocaleString('pt-BR')
+    .replace('.', '')
+    .replace(',', '.')}`;
+  currentPrice += price;
+  priceSaver(totalPrice.innerHTML, currentPrice);
+};
+
+const cartAdder = async () => {
   const buttons = document.querySelectorAll('.item__add');
 
-  buttons.forEach((button) => {
-    button.addEventListener('click', async ({ target }) => {
-      const id = getSkuFromProductItem(target.parentNode);
-      const item = await fetchItem(id);
-      cart.appendChild(createCartItemElement(item));
-      saveCartItems(cart.innerHTML);
+  try {
+    buttons.forEach((button) => {
+      button.addEventListener('click', async ({ target }) => {
+        const id = await getSkuFromProductItem(target.parentNode);
+        const item = await fetchItem(id);
+        cart.appendChild(createCartItemElement(item));
+        saveCartItems(cart.innerHTML);
+        priceSum(item.price);
+      });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const cartCleaner = () => {
   const button = document.querySelector('.empty-cart');
   const cartItems = document.querySelector('.cart__items');
+  const basePrice = 0;
+  const totalPrice = document.querySelector(totalPriceClass);
   button.addEventListener('click', () => {
     cartItems.innerHTML = '';
     localStorage.clear();
+    totalPrice.innerText = `${basePrice.toLocaleString('pt-BR')}`;
+    currentPrice = 0;
   });
 };
 
@@ -89,7 +149,9 @@ const setSavedCartItems = () => {
 
 window.onload = async () => {
   await listCreator();
+  totalPriceCreator();
   cartAdder();
   cartCleaner();
   setSavedCartItems();
+  priceCatcher();
 };
